ba82a1f23d9d27e0abc1655affb2aa1e891d9d68
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2023-05-31 21:32:15 +0900

    Fix: Stage4 added build settings

--- unityModule01/Assets/Scripts/GameManager.cs
+++ unityModule01/Assets/Scripts/GameManager.cs
@@ -11,17 +11,12 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance;
     public static GameManager Instance => _instance;
 
-    private readonly int _maxIndex = 3;
-    private int _stageIndex = 3;
+    private readonly int _maxIndex = 4;
+    private int _stageIndex = 4;
 
     private GameObject _playerGroup;
     private int _totalCount;
     private int _exitCount;
-
-    [SerializeField] private Material blue;
-    [SerializeField] private Material yellow;
-    [SerializeField] private Material red;
-    private Dictionary<Color, Material> _colorDictionary = new ();
 
     private bool IsAllExited => _exitCount >= _totalCount;
     public GameObject PlayerGroup => _playerGroup;
@@ -43,33 +38,12 @@ public class GameManager : MonoBehaviour
         }
         
         InitExitState();
-        _colorDictionary[Color.Blue] = blue;
-        _colorDictionary[Color.Yellow] = yellow;
-        _colorDictionary[Color.Red] = red;
     }
 
     #endregion
 
     #region Public Methods
 
-    public Material GetMaterial(Color color) => _colorDictionary[color];
-
-    public int GetLayer(Color color)
-    {
-        switch (color)
-        {
-            case Color.Blue:
-                return 7;
-            case Color.Yellow:
-                return 9;
-            case Color.Red:
-                return 11;
-            case Color.Default:
-            default:
-                return 0;
-        }
-    }
-    
     public void AlignPlayerExit(string name)
     {
         _exitCount++;
