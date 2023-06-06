776c390015d0151e0c9437790873901cf78c5ff7
sunhkim <jsg2804@gmail.com>
2023-06-06 15:49:56 +0900

    Feat: not enough point warning text

---
+++ unityModule05/Assets/Scripts/UI/StageUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Module04
+{
+    public class StageUI : MonoBehaviour
+    {
+        private TextMeshProUGUI _warningText;
+        private Color _textColor;
+        private float _fadeTime = 1f;
+
+        private void Awake()
+        {
+            _warningText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+            _warningText.gameObject.SetActive(false);
+        }
+
+        private void Start()
+        {
+            GameManager.Instance.OnPointNotEnough += DisplayWarningText;
+        }
+
+        private void OnDestory()
+        {
+            GameManager.Instance.OnPointNotEnough -= DisplayWarningText;
+        }
+
+        private void DisplayWarningText()
+        {
+            _warningText.gameObject.SetActive(true);
+            StartCoroutine(TextDisapear());
+        }
+
+        IEnumerator TextDisapear()
+        {
+            _textColor = Color.black;
+            while (_textColor.a >= 0f)
+            {
+                _textColor.a -= _fadeTime * Time.deltaTime;
+                _warningText.color = _textColor;
+                yield return null;
+            }
+
+            _warningText.gameObject.SetActive(false);
+        }
+    }
+}
