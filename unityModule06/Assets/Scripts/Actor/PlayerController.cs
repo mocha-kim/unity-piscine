a8b1e60860b9dd8c2deff5f3683e5fb1f2f3d5cf
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2023-06-09 01:54:29 +0900

    Feat: camera transition

--- unityModule06/Assets/Scripts/Actor/PlayerController.cs
+++ unityModule06/Assets/Scripts/Actor/PlayerController.cs
@@ -5,16 +5,22 @@ namespace Actor
 {
     public class PlayerController : MonoBehaviour
     {
+        private bool _isFPSMode = false;
+
+        private readonly float _rotateSpeed = 4f;
         private readonly float _speed = 5f;
         private Vector3 _dirVector = Vector3.zero;
-        
+
+        private GameObject _body;
         private CharacterController _controller;
 
         private int _speedIndex;
         private Animator _animator;
+        [SerializeField] private GameObject _TPSCamera;
 
         private void Awake()
         {
+            _body = transform.GetChild(0).gameObject;
             _controller = GetComponent<CharacterController>();
 
             _speedIndex = Animator.StringToHash("speed");
@@ -23,11 +29,29 @@ namespace Actor
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                _TPSCamera.SetActive(_isFPSMode);
+                _body.SetActive(_isFPSMode);
+                _isFPSMode = !_isFPSMode;
+            }
+            
             _dirVector.x = Input.GetAxis("Horizontal");
             _dirVector.z = Input.GetAxis("Vertical");
-            if (_dirVector != Vector3.zero)
-                transform.forward = _dirVector;
+            if (_isFPSMode)
+            {
+                float yRotateSize = Input.GetAxis("Mouse X") * _rotateSpeed;
+                float yRotate = transform.eulerAngles.y + yRotateSize;
 
+                transform.eulerAngles = new Vector3(0f, yRotate, 0f);
+
+                _dirVector = transform.forward * _dirVector.z + transform.right * _dirVector.x;
+            }
+            else
+            {
+                if (_dirVector != Vector3.zero)
+                    transform.forward = _dirVector;
+            }
             _controller.Move(_dirVector * _speed * Time.deltaTime);
             _animator.SetFloat(_speedIndex, _dirVector.magnitude);
         }
